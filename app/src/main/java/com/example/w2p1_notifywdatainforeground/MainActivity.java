9598cbdc620a76912495a20d30167cf66fce551d
Emmanuel Farrar <6923873+emmanuelfarrar@users.noreply.github.com>
2020-05-18 19:58:07 +0300

    Added requirements for assignment
    Added requirements for assignment, notifications cn be sent and recieved.

--- app/src/main/java/com/example/w2p1_notifywdatainforeground/MainActivity.java
+++ app/src/main/java/com/example/w2p1_notifywdatainforeground/MainActivity.java
@@ -1,14 +1,63 @@
 package com.example.w2p1_notifywdatainforeground;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
+import com.google.firebase.messaging.FirebaseMessaging;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn_LogToken = findViewById(R.id.logTokenButton);
+
+        /**
+         * Generating new token
+         */
+       btn_LogToken.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               FirebaseInstanceId.getInstance().getInstanceId()
+                       .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+                           @Override
+                           public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                               if (!task.isSuccessful()) {
+                                   Log.w(TAG, "getInstanceId failed", task.getException());
+                                   return;
+                               }
+
+                               // Get new Instance ID token
+                               String token = task.getResult().getToken();
+
+                               // Log and toast
+                               String msg = getString(R.string.msg_token_fmt, token);
+                               Log.d(TAG, msg);
+                               Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+                           }
+                       });
+           }
+       });
+    }
+
+    public void runtimeEnableAutoInit() {
+        // [START fcm_runtime_enable_auto_init]
+        FirebaseMessaging.getInstance().setAutoInitEnabled(true);
+        // [END fcm_runtime_enable_auto_init]
     }
 }
