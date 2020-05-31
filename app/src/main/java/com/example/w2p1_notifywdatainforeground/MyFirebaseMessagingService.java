272bf31cc2e3c19e07700ecd1c36910c510569af
Emmanuel Farrar <6923873+emmanuelfarrar@users.noreply.github.com>
2020-05-31 05:05:17 +0300

    Still not working

--- app/src/main/java/com/example/w2p1_notifywdatainforeground/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/w2p1_notifywdatainforeground/MyFirebaseMessagingService.java
@@ -2,6 +2,9 @@ package com.example.w2p1_notifywdatainforeground;
 
 import android.util.Log;
 
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkManager;
+
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
@@ -22,6 +25,15 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         // Check if message contains a data payload.
         if (remoteMessage.getData().size() > 0) {
             Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+
+            if (/* Check if data needs to be processed by long running job */ true) {
+                // For long-running tasks (10 seconds or more) use Firebase Job Dispatcher.
+                scheduleJob();
+            } else {
+                // Handle message within 10 seconds
+                handleNow();
+            }
+
         }
 
         // Check if message contains a notification payload.
@@ -33,6 +45,23 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         // message, here is where that should be initiated. See sendNotification method below.
     }
 
+    /**
+     * Schedule async work using WorkManager.
+     */
+    private void scheduleJob() {
+        // [START dispatch_job]
+        OneTimeWorkRequest work = new OneTimeWorkRequest.Builder(MyWorker.class)
+                .build();
+        WorkManager.getInstance().beginWith(work).enqueue();
+        // [END dispatch_job]
+    }
+
+    /**
+     * Handle time allotted to BroadcastReceivers.
+     */
+    private void handleNow() {
+        Log.d(TAG, "Short lived task is done.");
+    }
 
 
 }   // [END MyFirebaseMessagingService]
